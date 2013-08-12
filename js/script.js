e05cf03c80e39c10df75d2a1be0afa4d31387a6d
Subramaniam Sahoo <subramaniam.sahoo@onebcg.com>
2013-08-12 17:37:27 +0530

    Post data to server using backbone js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-  (function($) {
-
-    var Photo = Backbone.Model.extend({
-      urlRoot: 'test',
-      //defaults: {'imgsrc': 'test.jpg', 'title': 'test title'},
-      validate: function(attributes) {
-        if (attributes.title === undefined)
-          return "Title blannk error";
-      },
-      initialize: function() {
-        this.on('change:imgsrc', function() {
-          // console.log(this.get('imgsrc') + ' changes');
-        });
-        this.on('invalid', function(model, error) {
-          console.log(error);
-        });
-      },
-      changeSrc: function(source) {
-        this.set('imgsrc', source);
-      }
-    });
-
-    var photoObj = new Photo;
-    
-   // photo = {imgsrc: 'testsss.jpg',title:'title'};
-    photo = {id:1};
-    photoObj.save(photo, {
-      success: function(photo) {
-        consolr.log(photo.toJSON());
-      }
-    });
-    
-  })(jQuery);
+//  (function($) {
+//
+//    var Photo = Backbone.Model.extend({
+//      urlRoot: 'test',
+//      //defaults: {'imgsrc': 'test.jpg', 'title': 'test title'},
+//      validate: function(attributes) {
+//        if (attributes.title === undefined)
+//          return "Title blannk error";
+//      },
+//      initialize: function() {
+//        this.on('change:imgsrc', function() {
+//          // console.log(this.get('imgsrc') + ' changes');
+//        });
+//        this.on('invalid', function(model, error) {
+//          console.log(error);
+//        });
+//      },
+//      changeSrc: function(source) {
+//        this.set('imgsrc', source);
+//      }
+//    });
+//
+//    var photoObj = new Photo({id:1,title:'Title'});
+//    
+//   // photo = {imgsrc: 'testsss.jpg',title:'title'};
+//   // photo = {id:1};
+//    photoObj.save( {
+//      success: function(photo) {
+//        console.log(photo.toJSON());
+//      }
+//    });
+//    
+//  })(jQuery);
