8bcbd2ba024ed12ba90fb03178bc4885db521caf
Subramaniam Sahoo <subramaniam.sahoo@onebcg.com>
2013-08-12 14:48:30 +0530

    Chnage event as per the specific attribute

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
   (function($) {
     
     var Photo = Backbone.Model.extend({
-      defaults: {'imgsrc': 'test.jpg'},
+      defaults: {'imgsrc': 'test.jpg', 'title':'test title'},
       initialize: function() {
       
-        this.on('change', function() {
+        this.on('change:imgsrc', function() {
          
-          console.log( this.get('imgsrc'));
+          console.log( this.get('imgsrc')+' changes');
         });
 
       },
