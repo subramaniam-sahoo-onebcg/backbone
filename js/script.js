d7bebb6a5aa3772b5fa8875902cc35e296949b0d
Subramaniam Sahoo <subramaniam.sahoo@onebcg.com>
2013-08-12 16:10:13 +0530

    Chnage event as per the specific attribute

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
   (function($) {
-    
-    var Photo = Backbone.Model.extend({
-      defaults: {'imgsrc': 'test.jpg', 'title':'test title'},
-      initialize: function() {
-      
-        this.on('change:imgsrc', function() {
-         
-          console.log( this.get('imgsrc')+' changes');
-        });
 
+    var Photo = Backbone.Model.extend({
+      //defaults: {'imgsrc': 'test.jpg', 'title': 'test title'},
+      validate: function(attributes) {
+        if (attributes.title === undefined)
+          return "Title blannk error";
+      },
+      initialize: function() {
+        this.on('change:imgsrc', function() {
+          // console.log(this.get('imgsrc') + ' changes');
+        });
+        this.on('invalid', function(model, error) {
+          console.log(error);
+        });
       },
       changeSrc: function(source) {
         this.set('imgsrc', source);
@@ -16,5 +20,6 @@
     });
 
     var photo = new Photo;
-    photo.changeSrc('testsss.jpg');
+    photo.set({imgsrc: 'testsss.jpg'}, {validate: true});
+
   })(jQuery);
